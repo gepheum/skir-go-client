f9cbab5c48bbf8e50dd8c6354eaf477eb2c82eb9
Tyler Fibonacci <gepheum@gmail.com>
2026-04-01 21:57:21 -0400

    s/Skir service/SkirRPC service

--- method.go
+++ method.go
@@ -1,7 +1,7 @@
 package skir_client
 
-// Method identifies one method in a Skir service, both on the server side and
-// the client side.
+// Method identifies one method in a SkirRPC service, both on the server side
+// and the client side.
 //
 //   - Request is the type of the request parameter.
 //   - Response is the type of the response returned by this method.
--- service.go
+++ service.go
@@ -326,7 +326,7 @@ func (b *ServiceBuilder[RequestMeta]) Build() *Service[RequestMeta] {
 // If you are using the standard Go HTTP library, prefer
 // [Service.HandleRequestFromStandardLib], which extracts the body from an
 // *http.Request automatically. HandleRequest is a lower-level alternative for
-// integrating a Skir service into a different web framework.
+// integrating a SkirRPC service into a different web framework.
 func (s *Service[RequestMeta]) HandleRequest(ctx context.Context, body string, meta RequestMeta) RawResponse {
 	switch body {
 	case "", "studio":
@@ -406,7 +406,7 @@ const studioHtmlTemplate = `<!DOCTYPE html>
   <head>
     <meta charset="utf-8" />
     <title>RPC Studio</title>
-    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%%22http://www.w3.org/2000/svg%%22 viewBox=%%220 0 100 100%%22><text y=%%22.9em%%22 font-size=%%2290%%22>⚡</text></svg>">
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%%22http://www.w3.org/2000/svg%%22 viewBox=%%220 0 100 100%%22><text y=%%22.9em%%22 font-size=%%2290%%22>🐙</text></svg>">
     <script src="%s"></script>
   </head>
   <body style="margin: 0; padding: 0;">
